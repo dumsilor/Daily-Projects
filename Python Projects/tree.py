d27458e772f5a2d5089e2a61cb1aabc9f6ed91fb
Mosiur Rahman <dum.silor@gmail.com>
2021-05-29 10:09:09 +0600

    No Changes

---
+++ Python Projects/tree.py
@@ -0,0 +1,15 @@
+import os
+
+base_dir = r'C:\\'
+
+
+def is_file (conten_name):
+    
+
+def file_list (directory):
+    list_file = os.listdir(directory)
+    for content in list_file:
+        print(content) 
+
+
+file_list(base_dir)
